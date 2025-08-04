741e556d39522e5de7cd152c865a4228cd395a2e
Digvijay Uddhav Mane <manedigvijay00@gmail.com>
2025-08-04 22:12:03 +0530

    Add new practice questions under JavaBasics

---
+++ 01_JavaBasics/Practice_Questions/Ouestion2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ouestion2 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side of Square: ");
+        int side = sc.nextInt();
+        int Area = side*side;
+        System.out.println("Area of Square: "+Area);
+
+    }
+    
+}
---
+++ 01_JavaBasics/Practice_Questions/Question1.java
@@ -0,0 +1,14 @@
+package Practice_Questions;
+
+import java.util.*;
+
+public class Question1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int average = (a+b+c)/3;
+        System.out.println("Average of three numbers: "+average);
+    }  
+}
