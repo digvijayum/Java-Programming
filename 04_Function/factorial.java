ed37691650780f146d9e2df13fd5b020e560af6e
Digvijay Uddhav Mane <manedigvijay00@gmail.com>
2025-08-08 22:14:23 +0530

    Add New Java File

---
+++ 04_Function/factorial.java
@@ -0,0 +1,13 @@
+public class factorial {
+    public static int Factorial(int n){
+        int fact = 1;
+         
+        for(int i=1; i<=n; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        System.out.println((Factorial(4)));
+    }
+}
