47353de1ad7036a0feed3359d39b97734cf50d48
Digvijay Uddhav Mane <manedigvijay00@gmail.com>
2025-08-15 20:58:37 +0530

    Add Butterfly.java file

---
+++ 04_Function/Patterns Part II/Butterfly.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Butterfly {
+
+    public static void Butterfly_Pattern(int n){
+        //1st Half
+        //outer loop
+        for(int i=1; i<=n; i++){
+            //inner loop
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces- 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //2nd Half
+        //outer loop
+        for(int i=n; i>=1; i--){
+            //inner loop
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces- 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Butterfly_Pattern(4);
+    }
+}
