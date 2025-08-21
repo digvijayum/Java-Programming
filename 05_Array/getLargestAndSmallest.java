60a9e2452ed0fa60377027a0ab7e7c66aa080917
Digvijay Uddhav Mane <manedigvijay00@gmail.com>
2025-08-21 19:32:28 +0530

    Add getLargestAndSmallest.java file

---
+++ 05_Array/getLargestAndSmallest.java
@@ -0,0 +1,25 @@
+public class getLargestAndSmallest {
+
+    public static int getLargestAndSmallest(int numbers[]){
+        int Largest = Integer.MIN_VALUE; //-infinity
+        int Smallest = Integer.MAX_VALUE; //+infinity
+
+        for(int i=0; i<numbers.length; i++){
+            if(Largest<numbers[i]){
+                Largest = numbers[i];
+            }
+
+            if(Smallest>numbers[i]){
+                Smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest Number: "+Smallest);
+        return Largest;
+    }
+    public static void main(String[] args){
+        int numbers[] = {1, 2, 4, 6, 8, 3};
+
+        System.out.println("Largest Number: "+getLargestAndSmallest(numbers));
+
+    }
+}
